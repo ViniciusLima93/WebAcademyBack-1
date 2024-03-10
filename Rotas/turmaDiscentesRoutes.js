b5fdfa4ad4313d687024782f957ceb3a094476f7
JuanSoftware <92263142+JuanSoftware@users.noreply.github.com>
2024-03-10 18:51:30 -0400

    Adicionei o get por id

--- Rotas/turmaDiscentesRoutes.js
+++ Rotas/turmaDiscentesRoutes.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../db');
-
-router.get('/materia/:idMateria', (req, res) => {
-    const idMateria = req.params.idMateria;
-    const query = 'SELECT Matricula_Discente FROM Turma_Discente WHERE ID_Turma IN (SELECT ID_Turma FROM Turma WHERE ID_Materia = ?)';
-
-    db.query(query, [idMateria], (err, result) => {
-        if (err) {
-            res.status(500).send("Erro ao obter discentes matriculados na matéria");
-        } else {
-            res.json(result);
-        }
-    });
-});
-
 router.post('/', (req, res) => {
     const { ID_Turma, Matricula_Discente } = req.body;
 
@@ -31,20 +14,56 @@ router.post('/', (req, res) => {
     });
 });
 
-// Read (Ler)
+
 router.get('/', (req, res) => {
     const query = "SELECT * FROM Turma_Discente";
 
     db.query(query, (err, result) => {
         if (err) {
-            res.status(500).send("Erro ao obter relação turma-discente");
+            console.error("Erro ao obter discentes nas turmas:", err);
+            res.status(500).send("Erro ao obter discentes nas turmas");
         } else {
             res.json(result);
         }
     });
 });
 
-// Delete (Apagar)
+
+router.get('/:idTurma', (req, res) => {
+    const idTurma = req.params.idTurma;
+
+    const query = `SELECT * FROM Turma_Discente WHERE ID_Turma = ${idTurma}`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            console.error("Erro ao obter discentes da turma:", err);
+            res.status(500).send("Erro ao obter discentes da turma");
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+
+router.put('/:idTurma/:matriculaDiscente', (req, res) => {
+    const { idTurma, matriculaDiscente } = req.params;
+    const { novoID_Turma, novaMatricula_Discente } = req.body;
+
+    const query = `UPDATE Turma_Discente 
+                   SET ID_Turma=${novoID_Turma}, Matricula_Discente='${novaMatricula_Discente}' 
+                   WHERE ID_Turma=${idTurma} AND Matricula_Discente='${matriculaDiscente}'`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            console.error("Erro ao atualizar relação turma-discente:", err);
+            res.status(500).send('Erro ao atualizar relação turma-discente');
+        } else {
+            console.log("Relação turma-discente atualizada com sucesso:", result);
+            res.status(200).send('Relação turma-discente atualizada com sucesso');
+        }
+    });
+});
+
 router.delete('/:idTurma/:matriculaDiscente', (req, res) => {
     const { idTurma, matriculaDiscente } = req.params;
 
