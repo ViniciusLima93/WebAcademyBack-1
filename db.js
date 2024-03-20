5cac24d2f541531cc396b43d2b444920a99106b6
vinicius.lima <vinicius.lima@grupoicts.com.br>
2024-03-20 16:41:26 -0300

    ajuste de banco-tabela docente

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password : 'Vini!2502',
+        password : 'root',
         database: 'mapinguari',
     }
 );
